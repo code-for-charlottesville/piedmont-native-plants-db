243c7ea35701509e90d6e58d3c7908eb7eb21da6
Eric Schorling <eric.schorling@yahoo.com>
2021-10-26 17:46:25 -0400

    added some content to display and updated cards, created some initial thoughts on search and advanced search layouts

---
+++ client/src/elements/searchOptionsObject.js
@@ -0,0 +1,17 @@
+export const searchOptionsObject ={
+    region:[
+        "albemarle county",
+        "charlottesville city",
+        "buckingham",
+        "culpeper",
+        "fluvanna",
+        "greene",
+        "louisa",
+        "madison",
+        "nelson county",
+        "orange",
+        "virginia",
+        "united states",
+        "disputed"
+    ]
+}
