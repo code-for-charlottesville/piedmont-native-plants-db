57bfc782b5353352f2826e215d88f90587d2870a
adrielk <theadrielkim@gmail.com>
2022-03-30 11:47:20 -0400

    front-end

---
+++ piedmont-plants-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
