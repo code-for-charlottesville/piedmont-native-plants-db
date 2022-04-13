6d5d12ed0e9650358c79585125a15fad0944097d
adrielk <theadrielkim@gmail.com>
2022-04-13 17:30:28 -0400

    Filtering for Categories and Bloom Dates

--- backend/routes/plants.js
+++ backend/routes/plants.js
@@ -1,6 +1,6 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const QueryPromise = require('../database/dbService');
+const QueryPromise = require("../database/dbService");
 
 const sendAll = (res) => {
   QueryPromise("SELECT * FROM plant_core")
@@ -10,11 +10,26 @@ const sendAll = (res) => {
     .catch((err) => {
       res.send(err);
     });
-}
+};
 
 /* GET plants listing. */
-router.get('/', (req, res, next) => sendAll(res));
-router.get("/all", (req, res, next) => sendAll(res));
+router.get("/", (req, res, next) => sendAll(res));
+router.get("/all", (req, res, next) => {
+  let bloom_filter = req.query.bloom ? req.query.bloom : null; //string
+  let cat_filter = req.query.category ? req.query.category : "(4,6,8,14,13,3)"; //need a better way to include all categorys if this parameter is omitted.
+  //also have to filter by category (natural join by category )
+  // there must be an easier way to do this....
+  console.log(bloom_filter);
+  QueryPromise(
+    `SELECT * FROM plant_core  
+    WHERE ((${bloom_filter} IS NULL) OR (bloom_months = ${bloom_filter}))
+    AND (category_id IN ${cat_filter})`
+  )
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => res.send(err));
+});
 
 router.get("/:id", function (req, res, next) {
   //res.send("respond with a resource");
@@ -28,4 +43,3 @@ router.get("/:id", function (req, res, next) {
 });
 
 module.exports = router;
-
