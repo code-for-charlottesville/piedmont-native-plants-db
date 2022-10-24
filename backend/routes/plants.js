f954e8316c8ee169162179258c9ad4e1e38644d8
adrielk <theadrielkim@gmail.com>
2022-10-24 18:59:50 -0400

    dynamic filter request

--- backend/routes/plants.js
+++ backend/routes/plants.js
@@ -14,16 +14,19 @@ const sendAll = (res) => {
 
 /* GET plants listing. */
 router.get("/", (req, res, next) => sendAll(res));
-router.get("/all", (req, res, next) => {
-  let bloom_filter = req.query.bloom ? req.query.bloom : null; //string
-  let cat_filter = req.query.category ? req.query.category : "(4,6,8,14,13,3)"; //need a better way to include all categorys if this parameter is omitted.
-  //also have to filter by category (natural join by category )
-  // there must be an easier way to do this....
-  console.log(bloom_filter);
+
+router.get("/filter", (req, res, next) => {
+  let queryParams = req.query;
+  let numParams = Object.keys(queryParams).length;
+  let filter = Object.keys(queryParams).reduce((prev, cur, idx) => {
+    let comparison = `${cur} = ${queryParams[cur]}`;
+    return idx == numParams - 1
+      ? prev + comparison
+      : prev + comparison + " AND ";
+  }, "");
   QueryPromise(
-    `SELECT * FROM plant_core  
-    WHERE ((${bloom_filter} IS NULL) OR (bloom_months = ${bloom_filter}))
-    AND (category_id IN ${cat_filter})`
+    `SELECT * FROM plant_core
+    WHERE ${filter}`
   )
     .then((results) => {
       res.send(results);
@@ -31,6 +34,26 @@ router.get("/all", (req, res, next) => {
     .catch((err) => res.send(err));
 });
 
+//string search route. for all string fields, see if key word is contained look up how to do this
+
+// router.get("/all", (req, res, next) => {
+//   let bloom_filter = req.query.bloom ? req.query.bloom : null; //string
+//   let cat_filter = req.query.category ? req.query.category : "(4,6,8,14,13,3)"; //need a better way to include all categorys if this parameter is omitted.
+//   //also have to filter by category (natural join by category )
+//   // there must be an easier way to do this....
+//   console.log(bloom_filter);
+//   QueryPromise(
+//     `SELECT * FROM plant_core
+//     WHERE ((${bloom_filter} IS NULL) OR (bloom_months = ${bloom_filter}))
+//     AND (category_id IN ${cat_filter})`
+//   )
+//     .then((results) => {
+//       res.send(results);
+//     })
+//     .catch((err) => res.send(err));
+// });
+
+/* GET specific plant */
 router.get("/:id", function (req, res, next) {
   //res.send("respond with a resource");
   QueryPromise("SELECT * FROM plant_core WHERE plant_id = ?", [req.params.id])
