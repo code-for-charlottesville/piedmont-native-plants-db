03f98a83b3d76ae999de6f993f4dc26e2601a4c3
jonah <jonah.3.kim@gmail.com>
2023-06-12 15:07:01 -0400

    Moved template to root of project to be consistent with usual django project structure

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+import django.contrib.auth.views
+from django.urls import path, include
 import frontend.views as frontend_views
 
 app_name = 'frontend'
@@ -6,4 +7,5 @@ urlpatterns = [
     path('', frontend_views.home, name='base'),
     path('plants/', frontend_views.plant_list, name='plant_list'),
     path('plants/<int:plant_id>', frontend_views.plant_detail, name='plant_detail'),
+    path('account/', include('django.contrib.auth.urls'))
 ]
--- frontend/views/views.py
+++ frontend/views/views.py
@@ -5,12 +5,12 @@ from backend.models import Plant
 
 # Create your views here.
 def home(request):
-    return render(request, 'components/base.html')
+    return render(request, 'frontend/components/base.html')
 
 
 def plant_list(request):
-    return render(request, 'plant_list.html', context={'plants': Plant.objects.all()})
+    return render(request, 'frontend/plant_list.html', context={'plants': Plant.objects.all()})
 
 def plant_detail(request, plant_id):
-    return render(request, 'plant_detail.html', context={'plant': get_object_or_404(Plant, id=plant_id)})
+    return render(request, 'frontend/plant_detail.html', context={'plant': get_object_or_404(Plant, id=plant_id)})
 
