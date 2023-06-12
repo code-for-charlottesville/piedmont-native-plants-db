033d86f1adc8b598dabc226dc9cd3fbdad2f09ed
jonah <jonah.3.kim@gmail.com>
2023-06-12 14:47:07 -0400

    Made simple plant detail view

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,5 +4,6 @@ import frontend.views as frontend_views
 app_name = 'frontend'
 urlpatterns = [
     path('', frontend_views.home, name='base'),
-    path('list/', frontend_views.plant_list, name='list')
+    path('plants/', frontend_views.plant_list, name='plant_list'),
+    path('plants/<int:plant_id>', frontend_views.plant_detail, name='plant_detail'),
 ]
--- frontend/views/views.py
+++ frontend/views/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from backend.models import Plant
 
@@ -10,3 +10,7 @@ def home(request):
 
 def plant_list(request):
     return render(request, 'plant_list.html', context={'plants': Plant.objects.all()})
+
+def plant_detail(request, plant_id):
+    return render(request, 'plant_detail.html', context={'plant': get_object_or_404(Plant, id=plant_id)})
+
