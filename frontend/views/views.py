5c1a7f09af9e1e051a2a7d3a51b0058eb86aa290
jonah <jonah.3.kim@gmail.com>
2023-06-12 14:12:29 -0400

    Moved base.html

--- frontend/views/views.py
+++ frontend/views/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from backend.models import Plant
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'components/base.html')
+
+
+def plant_list(request):
+    return render(request, 'plant_list.html', context={'plants': Plant.objects.all()})
