930a4fa66a201f60d9922288ddf6d6182ad7e78a
jonah <jonah.3.kim@gmail.com>
2023-06-12 14:16:29 -0400

    Updated plant list for simple content display

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 import frontend.views as frontend_views
 
-app_name = 'website'
+app_name = 'frontend'
 urlpatterns = [
     path('', frontend_views.home, name='base'),
+    path('list/', frontend_views.plant_list, name='list')
 ]
