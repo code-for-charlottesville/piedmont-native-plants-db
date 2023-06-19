31af7a33c4daa358cc920d647af27fa5150798f4
jonah <jonah.3.kim@gmail.com>
2023-06-19 02:55:31 -0400

    Updated ApiTestCase.set_client_credentials to automatically set token if none is provided

--- backend/tests/ApiTestCase.py
+++ backend/tests/ApiTestCase.py
@@ -1,16 +1,29 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from knox.models import AuthToken
 from rest_framework.test import APIClient
 
 
 class ApiTestCase(TestCase):
     client: APIClient
+    user: User
 
     def setUp(self) -> None:
         self.client = APIClient()
+        self.user = User.objects.create_user(
+            username='testUser',
+            password='qwerty',
+            email='testUser@gmail.com'
+        )
 
-    def set_client_credentials(self, token_key):
-        """Specifies the auth token necessary to access `TokenAuthentication` based views"""
-        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token_key}')
+    def set_client_credentials(self, token_key=None):
+        """Sets the auth token needed to access `TokenAuthentication` based views.
+        If the token_key is not provided, the function will automatically authenticate the client."""
+        if token_key:
+            self.client.credentials(HTTP_AUTHORIZATION=f'Token {token_key}')
+        else:
+            token, key = AuthToken.objects.create(user=self.user)
+            self.client.credentials(HTTP_AUTHORIZATION=f'Token {key}')
 
     def assertAllIn(self, obj, *members):
         for member in members:
